832a89f7e8cd84400d4d7f2969e5b6096d80b47f
eloiseyap <124944869+eloiseyap@users.noreply.github.com>
2023-08-11 06:53:50 +0000

    movement

--- game.py
+++ game.py
@@ -1,6 +1,33 @@
 # Import statements
-import data
+from data import Player, Zonemap
 
 class MUDGame:
     def __init__(self):
-        pass
+        self.start = 0
+        self.end = 10
+        self.player = Player()
+        self.map = Zonemap()
+    
+
+    def movement(self):
+        if self.start == self.end:
+                print("You have reached the end!")
+        for i in range(10):
+            rooms = []
+            if self.start == self.map.keys():
+                rooms.append(self.map[str(i)])
+        print("You can move to room(s)"+ str(rooms)+'.')
+        next = input("Which room do you wish to move to?")
+        if next not in str(rooms):
+            print("Unable to move here!")
+        else:
+            self.start = int(next)
+
+    
+                    
+               
+        
+        
+                
+
+   
