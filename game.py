8515aa53e7ca5bec4f008448c004e90cddb022cf
eloiseyap <124944869+eloiseyap@users.noreply.github.com>
2023-08-14 01:32:21 +0000

    edited movement

--- game.py
+++ game.py
@@ -13,16 +13,17 @@ class MUDGame:
     def movement(self):
         if self.start == self.end:
                 print("You have reached the end!")
-        for i in range(10):
-            rooms = []
-            if self.start == self.map.keys(i):
-                rooms.append(self.map[str(i)])
-        print("You can move to room(s)"+ str(rooms)+'.')
-        next = input("Which room do you wish to move to?")
-        if next not in str(rooms):
-            print("Unable to move here!")
         else:
-            self.start = int(next)
+            for i in range(10):
+                rooms = []
+                if self.start == self.map.keys(i):
+                    rooms.append(self.map[str(i)])
+            print("You can move to room(s)"+ str(rooms)+'.')
+            next = input("Which room do you wish to move to?")
+            if next not in str(rooms):
+                print("Unable to move here!")
+            else:
+                self.start = int(next)
 
     def run(self, Player, Enemy):
         self.player.set_username()
