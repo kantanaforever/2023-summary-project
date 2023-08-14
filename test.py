d7414db716400f834c4f5436449ee6fb1ce5de05
WinterStar17 <125122261+WinterStar17@users.noreply.github.com>
2023-08-14 01:40:35 +0000

    updated test.py

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 # Import statements
 from main import game
+from data import Zonemap
+import zonemap.json
 
 
 def test_game():
     # Write code to test the game object here
     # Raise an error if the test fails
+    Zonemap(zonemap.json)
+    print(game.MUDGame.movement())
     pass
 
 test_game()
