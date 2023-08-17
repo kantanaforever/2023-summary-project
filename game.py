afde28342a60407c90e3b63aee39ccecb7acad13
eloiseyap <124944869+eloiseyap@users.noreply.github.com>
2023-08-17 06:49:50 +0000

    idk

--- game.py
+++ game.py
@@ -5,9 +5,9 @@ class MUDGame:
     def __init__(self):
         self.start = 0
         self.end = 10
-        self.player = data.Player(data.)
+        self.player = data.Player(data.map)
         self.enemy = data.Enemy()
-        self.map = data.Zonemap()
+        self.map = data.map
         self.inventory = data.Inventory()
 
     def movement(self):
@@ -26,19 +26,17 @@ class MUDGame:
                 self.start = int(next)
 
     def run(self, Player, Enemy):
+        """ defeat boss -> pick item  -> give option to use on next boss ?? """
         self.player.set_username()
         self.player.move()
-        
-        while self.hp > 0:
+        while self.player.hp or self.enemy.hp > 0:
             self.player.attack(self.enemy)
-        self.player.pick_item()
-        
-        self.inventory.use()
-        self.inventory.get_items()
-        self.player.consume_item()
-        
-        self.enemy.attack(self.player)
-        
+            self.enemy.attack(self.player)
+            if self.player.hp == 0:
+                return "GAME OVER!"
+                
+        else:
+            return "GAME OVER!"
         
         
                     
