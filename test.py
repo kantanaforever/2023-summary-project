53d6a4c0843e6935cee774fc644dc10b81507e1e
WinterStar17 <125122261+WinterStar17@users.noreply.github.com>
2023-08-21 07:33:16 +0000

    testing

--- test.py
+++ test.py
@@ -6,7 +6,19 @@ import data
 def test_game():
     # Write code to test the game object here
     # Raise an error if the test fails
-    game.movement()
+    # game.movement pass
+    # game.intro()
+    # game.set_username(data.Player()) pass
+    # game.room_desc(data.Player()) pass
+    # game.enemy_presence() pass
+    # game.inventory_show()
+    # game.inventory_consume_item()
+    # game.fight()
+    # game.item_presence()
+    # game.pick_item()
+    # game.final_room()
+    # game.win()
+    # game.run()
     pass
 
 test_game()
