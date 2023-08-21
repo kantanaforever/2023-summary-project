45fad3038561a6ae76277fddf9b5184171a6bb18
WinterStar17 <125122261+WinterStar17@users.noreply.github.com>
2023-08-21 08:05:01 +0000

    run 2.0

--- test.py
+++ test.py
@@ -6,19 +6,19 @@ import data
 def test_game():
     # Write code to test the game object here
     # Raise an error if the test fails
-    # game.movement pass
-    # game.intro()
+    # game.movement() pass
+    # game.intro() pass
     # game.set_username(data.Player()) pass
     # game.room_desc(data.Player()) pass
     # game.enemy_presence() pass
-    # game.inventory_show()
-    # game.inventory_consume_item()
+    # game.inventory_show() pass, but test again after picking up items function is done
+    game.inventory_consume_item()
     # game.fight()
     # game.item_presence()
     # game.pick_item()
     # game.final_room()
     # game.win()
-    game.run()
+    # game.run()
     pass
 
 test_game()
