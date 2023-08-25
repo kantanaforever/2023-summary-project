87a64f6e8a601c7bd61e7d4f382aa38665e3b62d
WinterStar17 <125122261+WinterStar17@users.noreply.github.com>
2023-08-25 06:12:05 +0000

    idk what is this..?

--- test.py
+++ test.py
@@ -6,23 +6,17 @@ import data
 def test_game():
     # Write code to test the game object here
     # Raise an error if the test fails
-    # game.movement() pass
-    # game.intro() pass
-    # game.set_username(data.Player()) pass
-    # game.room_desc(data.Player()) pass
-    # game.enemy_presence() pass
-    # game.inventory_show() pass, but test again after picking up items function is done
-    # game.inventory_consume_item() list is not callable
-    # enemy = data.Enemy()
-    # try:
-    #     game.player.attack_punch(enemy)
-    # except:
-    #     breakpoint()
-    # game.item_presence() keyerror 'item'
-    # game.pick_item() missing positional argument item_data
-    # game.final_room()
-    # game.win() no return yet
+    game.movement()
+    game.intro()
+    game.room_desc(data.Player())
+    game.set_username(data.Player())
+    game.inventory_show()
+    game.inventory_consume_item()
+    game.final_room()
+    game.final_boss_fight()
     # game.run()
+    print('There is no error in the code.')
     pass
+
 
 test_game()
