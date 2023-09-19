bc78811dbe353951e75e2d87fc0bd8219b4710c7
ngjunsiang <45561895+ngjunsiang@users.noreply.github.com>
2023-09-19 14:29:37 +0000

    Move test code to test.py

--- main.py
+++ main.py
@@ -4,8 +4,4 @@ from game import MUDGame
 game = MUDGame()
 
 if __name__ == "__main__":
-    import data
-    for item in data.generate_items():
-        game.player.take_item(item)
-    game.player.inventory.show()
-    # game.run()
+    game.run()
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-from main import game
+from game import MUDGame
 import data
 
 def test_game():
     # Write code to test the game object here
     # Raise an error if the test fails
+    game = MUDGame()
     game.movement()
     game.intro()
     game.room_desc()
@@ -12,5 +13,12 @@ def test_game():
     game.final_boss_fight()
     game.run()
     print('There is no error in the code.')
+
+def test_inventory_show():
+    game = MUDGame()
+    for item in data.generate_items():
+        game.player.take_item(item)
+    game.player.inventory.show()
     
 test_game()
+test_inventory_show()
