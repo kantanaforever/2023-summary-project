4e6719bc8e7a6f23dcf132a5faf9975b210a0095
ngjunsiang <45561895+ngjunsiang@users.noreply.github.com>
2023-09-19 14:27:08 +0000

    Test Inventory.show()

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ from game import MUDGame
 game = MUDGame()
 
 if __name__ == "__main__":
-    game.run()
+    import data
+    for item in data.generate_items():
+        game.player.take_item(item)
+    game.player.inventory.show()
+    # game.run()
